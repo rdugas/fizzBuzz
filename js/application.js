5829a8f9c8c51d14ba0ecf803734ffc7c2edf838
Rob Dugas <rob.dugas@gmail.com>
2014-12-31 01:35:03 -0600

    fix output for each branch of logic

--- js/application.js
+++ js/application.js
@@ -2,19 +2,20 @@ $(document).ready(function(){
 	alert("let's get buzzed");
 
 	for (var myNumber = 1; myNumber <= 100; myNumber++) {
-			
-				
-				if (dividedByEvenly(myNumber, 3)){
-					$("body").append("fizz");
-				}
-				else if (dividedByEvenly(myNumber, 5)){
-					$("body").append(myNumber);
-				}
-				else {
-					$("body").append(myNumber);
-				}
 
-			}
+		if (dividedByEvenly(myNumber, 3) && dividedByEvenly(myNumber, 5)) {
+			$("body").append("fizz buzz");
+		}
+		else if (dividedByEvenly(myNumber, 3)){
+			$("body").append("fizz");
+		}
+		else if (dividedByEvenly(myNumber, 5)){
+			$("body").append("buzz");
+		}
+		else {
+			$("body").append(myNumber);
+		}
+	}
 
 	function dividedByEvenly(numerator, denominator){
 		if (numerator % denominator == 0)
