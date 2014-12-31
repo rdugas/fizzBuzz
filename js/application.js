69fd0a3ff0ae0a55f3dfc1fb6cb07314983dee14
Rob Dugas <rob.dugas@gmail.com>
2014-12-31 01:30:15 -0600

    add divided evenly function and for loop

--- js/application.js
+++ js/application.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-	alert("ready set shop");
+	alert("let's get buzzed");
+
+	for (var myNumber = 1; myNumber <= 100; myNumber++) {
+			
+				
+				if (dividedByEvenly(myNumber, 3)){
+					$("body").append("fizz");
+				}
+				else if (dividedByEvenly(myNumber, 5)){
+					$("body").append(myNumber);
+				}
+				else {
+					$("body").append(myNumber);
+				}
+
+			}
+
+	function dividedByEvenly(numerator, denominator){
+		if (numerator % denominator == 0)
+			{return true};
+	}
+
 });
