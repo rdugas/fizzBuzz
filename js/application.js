52026ec89b4c620fceff5f8ca4bc7cf03a4708fe
Rob Dugas <rob.dugas@gmail.com>
2014-12-31 01:59:42 -0600

    change html so signature at bottom. Change js to append to section not body

--- js/application.js
+++ js/application.js
@@ -4,19 +4,19 @@ $(document).ready(function(){
 	for (var myNumber = 1; myNumber <= 100; myNumber++) {
 
 		if (dividedByEvenly(myNumber, 3) && dividedByEvenly(myNumber, 5)) {
-			$("body").append("FIZZ BUZZ, ");
+			$("section").append("FIZZ BUZZ, ");
 		}
 		else if (dividedByEvenly(myNumber, 3)){
-			$("body").append("fizz, ");
+			$("section").append("fizz, ");
 		}
 		else if (myNumber == 100){
-			$("body").append("buzz. THE END!!!");
+			$("section").append("buzz. THE END!!!");
 		}
 		else if (dividedByEvenly(myNumber, 5)){
-			$("body").append("buzz, ");
+			$("section").append("buzz, ");
 		}
 		else {
-			$("body").append(myNumber + " ,");
+			$("section").append(myNumber + " ,");
 		}
 	};
 
